9376012dbdc8865bd257b94a15ab1932f4e87384
tangjm <jaredtang@hotmail.co.uk>
2022-06-29 22:38:26 +0100

    Add QuickCheck tests on multiplication properites

---
+++ ch14_testing/testingMultiplication.hs
@@ -0,0 +1,16 @@
+module TestingMultiplication where
+
+import Test.QuickCheck
+
+associativity :: Int -> Int -> Int -> Bool
+associativity x y z = 
+  x * (y * z) == (x * y) * z
+
+commutativity :: Int -> Int -> Bool
+commutativity x y =
+  x * y == y * x
+
+main :: IO ()
+main = do
+  quickCheck associativity 
+  quickCheck commutativity
