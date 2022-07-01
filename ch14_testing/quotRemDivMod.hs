7d5d991a6eb32e693cbe7dd22f523112f7b9b9df
tangjm <jaredtang@hotmail.co.uk>
2022-07-01 17:47:04 +0100

    Update quotremDivMod.hs

--- ch14_testing/quotRemDivMod.hs
+++ ch14_testing/quotRemDivMod.hs
@@ -8,11 +8,22 @@ isPositive = (> 0)
 isNegative :: Int -> Bool
 isNegative = (< 0)
 
+-- We don't run tests for when the divisor is 0
 ignoreDivisionByZero :: (Int -> Int -> Bool) -> Int -> Int -> Maybe Bool
 ignoreDivisionByZero f x y
   | isPositive y || isNegative y = Just (f x y)
   | otherwise                    = Nothing
 
+
+positiveIntGen :: Gen Int
+positiveIntGen = elements [1..]
+
+negativeIntGen :: Gen Int
+negativeIntGen = elements [-1, -2 ..]
+
+intGen :: Gen Int
+intGen = frequency [ (1, positiveIntGen),
+                         (1, negativeIntGen) ]
 
 testQuotAndRem :: Int -> Int -> Bool
 testQuotAndRem x y =
@@ -26,5 +37,10 @@ main :: IO ()
 main = do
   quickCheck $ ignoreDivisionByZero testQuotAndRem
   quickCheck $ ignoreDivisionByZero testDivAndMod
+  
+  -- The following computation won't ever end because we have unbounded quantification.
+  -- quickCheck $ forAll intGen 
+  --               (\x -> forAll intGen 
+  --                 (\y -> testQuotAndRem x y))
 
 -- Fix 'division by zero error
