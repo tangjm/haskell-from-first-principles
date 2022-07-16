5ba6847c800beae759433f6774eaa5c297484c67
tangjm <jaredtang@hotmail.co.uk>
2022-07-16 11:43:25 +0100

    Update ThreeTraversable.hs to include another Traversable instance for a variant of the Three data type

--- ch21_traversable/ThreeTraversable.hs
+++ ch21_traversable/ThreeTraversable.hs
@@ -13,3 +13,30 @@ instance Foldable (Three a b) where
 
 instance Traversable (Three a b) where
   traverse f (Three x y z) = Three x y <$> f z 
+
+
+data Three' a b =
+  Three' a b b 
+
+instance Functor (Three' a) where
+  fmap f (Three' x y z) = Three' x (f y) (f z)
+
+
+-- instance Foldable (Three' a) where
+--   foldMap f (Three' x y z) = f z
+--   foldr f a (Three' x y z) = f z a 
+
+-- instance Foldable (Three' a) where 
+--   foldMap f (Three' x y z) = f y
+--   foldr f a (Three' x y z) = f y a
+
+instance Foldable (Three' a) where
+  foldMap f (Three' x y z) = f y <> f z
+  foldr f a (Three' x y z) = f y (f z a)
+
+-- n.b. 'f' is not only binary but also associative 
+-- There seems to be more than one way to implement the Foldable typeclass
+
+instance Traversable (Three' a) where
+  traverse f (Three' x y z) = Three' x <$> (f y) <*> (f z)
+  
